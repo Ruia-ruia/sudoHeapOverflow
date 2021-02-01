e78985929e3d5dc01b808035fb2c3bf90812b996
Ruia-ruia <61450241+Ruia-ruia@users.noreply.github.com>
2021-02-01 07:22:11 +0000

    Update bruteforcedrive.py

--- bruteforcedrive.py
+++ bruteforcedrive.py
@@ -5,7 +5,7 @@ while True:
         print("-----------------------")
         arg1 = random.randint(180,250)
         arg2 = random.randint(40,80)
-        arg3 = 11
+        arg3 = random.randint(10, 20)
         arg4 = random.randint(30,70)
         arg5 = random.randint(30,70)
         print("Executing: ./bruteforcehandle {} {} {} {} {}".format(arg1, arg2, arg3, arg4, arg5))
